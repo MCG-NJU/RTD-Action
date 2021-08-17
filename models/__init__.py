935db740223009389b826acb3568ab22d4a964ad
Jiaqi Tang <630251328@qq.com>
2021-08-17 19:37:24 +0800

    Revert "First commit."

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Modified from DETR (https://github.com/facebookresearch/detr)
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-# ------------------------------------------------------------------------
-from .rtd import build
-
-
-def build_model(args):
-    return build(args)
